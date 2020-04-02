3e625c753ed5724c41c7d60d850d531fba2f88dc
David Jakubec <david.jakubec@gmail.com>
2020-04-02 17:03:14 +0200

    Added testing directory. Initialized scripts.

--- code/INTAA-conservation.py
+++ code/INTAA-conservation.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
+from sys import argv
+
+
+def main(PDB_file, sequence_database):
+    pass
+
+
+if __name__ == '__main__':
+    main(*argv[1:])
+
 
---
+++ code/PDBReader.py
@@ -0,0 +1,11 @@
+
+
+def read_residue_names(PDB_file):
+    pass
+
+
+if __name__ == '__main__':
+    from sys import argv
+    read_residue_names(argv[1])
+
+
