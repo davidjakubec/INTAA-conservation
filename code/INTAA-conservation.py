7d5f3e3b7330a9e4f5257d74007d0e175f656ec7
David Jakubec <david.jakubec@gmail.com>
2020-04-11 15:15:25 +0200

    Added PDB parsing.

--- code/INTAA-conservation.py
+++ code/INTAA-conservation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 from sys import argv
+from _PDB_utils import read_chains
 
 
 def main(PDB_file, sequence_database):
-    pass
+    PDB_ID, chains = read_chains(PDB_file)
 
 
 if __name__ == '__main__':
--- code/_PDB_utils.py
+++ code/_PDB_utils.py
@@ -1,11 +1,27 @@
 
 
-def read_residue_names(PDB_file):
-    pass
+from Bio.PDB.PDBParser import PDBParser
+from Bio.PDB import Polypeptide
+
+
+def read_chains(PDB_file):
+    parser = PDBParser(PERMISSIVE=True)
+    PDB_ID = PDB_file.split('.')[0]
+    structure = parser.get_structure(PDB_ID, PDB_file)
+    model = structure[0]
+    chains = dict()
+    for chain in model:
+        residues = []
+        for residue in chain:
+            het, resseq, icode = residue.id
+            if (het == ' ') and (icode == ' '):
+                residues.append((residue.resname, resseq))
+        chains[chain.id] = residues
+    return PDB_ID, chains
 
 
 if __name__ == '__main__':
     from sys import argv
-    read_residue_names(argv[1])
+    PDB_ID, chains = read_chains(argv[1])
 
 
